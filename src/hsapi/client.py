d5e2a651d397d602df66461fbc0245e247a447df
Devan <mongolgod@gmail.com>
2022-02-21 03:35:53 -0800

    Add metadata and card classes

--- src/hsapi/client.py
+++ src/hsapi/client.py
@@ -2,12 +2,19 @@ from yaml import safe_load
 import requests
 from datetime import datetime,timedelta
 
-class BadCredentialsException(Exception):
+API_BASE_URL = 'https://us.api.blizzard.com/hearthstone'
+
+class OAuthAcquisitionException(Exception):
     """
-    `BadCredentialsException` is raised when the library is uanble to retrieve an OAuth token with the provided
-    credentials
+    `OAuthAcquisitionException` is raised when we are uanble to retrieve an OAuth token with the provided credentials
     """
     pass
+
+# TODO: Break this down into better errors
+class APIException(Exception):
+    """
+    `APIException` is raised when a 400 response is received when attempting to query the Hearthstone API
+    """
 
 
 class OAuthToken:
@@ -28,28 +35,13 @@ class HSApiClient:
     Hearthstone API client. Documentation can be found at
     https://develop.battle.net/documentation/hearthstone/game-data-apis
     """
-    API_BASE_URL = 'https://us.api.blizzard.com/hearthstone'
 
-    SEARCH_ALLOWED_FILTERS= [
-        "set",
-        "class",
-        "manaCost",
-        "attack",
-        "health",
-        "collectible",
-        "rarity",
-        "type",
-        "minionType",
-        "keyword",
-        "textFilter",
-        "gameMode",
-    ]
-
-    def __init__(self, region='us', locale='en_US'):
+    def __init__(self, region='us', locale='en_US', page_size=10):
         self.region = region
         self.locale = locale
+        self.page_size = page_size
 
-        with open('credentials.yml', 'r') as creds_file:
+        with open('../credentials.yml', 'r') as creds_file:
             credentials = safe_load(creds_file)
             self.client_id = credentials['client_id']
             self.client_secret = credentials['client_secret']
@@ -62,19 +54,66 @@ class HSApiClient:
             data={"grant_type": "client_credentials"})
         
         if not response.ok:
-            raise BadCredentialsException(f"Unable to obtain OAuth token: {response.content}")
-        
+            raise OAuthAcquisitionException(f"Unable to obtain OAuth token: {response.content}")
+
         response_json = response.json()
-        
         self.oauth_token = OAuthToken(response_json['access_token'], response_json['expires_in'])
+
+        # Retrieve HS metadata
+        self.metadata = HSApiMetadata(self.region, self.locale, self.oauth_token)
     
     def search(self, **args):
         args['access_token'] = self.oauth_token.token
         args['region'] = self.region
         args['locale'] = self.locale
-        response = requests.get(f"{self.API_BASE_URL}/cards", params=args)
+        args['pageSize'] = self.page_size
+        response = requests.get(f"{API_BASE_URL}/cards", params=args)
 
         if not response.ok:
-            # TODO: Error handling
-            pass
-        return response.json()['cards']
+            raise APIException(response.content)
+
+        return [ HearthstoneCard(self.metadata, **card) for card in response.json()['cards'] ]
+
+
+class HSApiMetadata:
+    """
+    Container class used to hold card metadata
+    """
+
+    def __init__(self, region, locale, oauth_token):
+        response = requests.get(
+            f"{API_BASE_URL}/metadata",
+            params={'access_token': oauth_token.token, 'locale': locale, 'region': region})
+        
+        if not response.ok:
+            raise APIException(response.content)
+        
+        response_json = response.json()
+        
+        # Store each metadata type in a dictionary by ID for faster lookup
+        self.types = { i['id']: i  for i in response_json['types'] }
+        self.rarities = { i['id']: i  for i in response_json['rarities'] }
+        self.classes = { i['id']: i  for i in response_json['classes'] }
+
+        # Sets need special logic to ahndle set aliasing
+        self.sets = {}
+        for i in response_json['sets']:
+            self.sets[i['id']] = i
+            if 'aliasSetIds' in i:
+                for j in i['aliasSetIds']:
+                    self.sets[j] = i
+
+
+class HearthstoneCard:
+    """
+    Internal representation of a Hearthstone card
+    """
+    def __init__(self, metadata, **args):
+        self.name = args['name']
+        self.art = args['image']
+        self.holo_art = args['imageGold']
+        self.set = metadata.sets[args['cardSetId']]['name']
+        self.card_class = metadata.classes[args['classId']]['name']
+        self.mana_cost = args['manaCost']
+        self.rarity = metadata.rarities[args['rarityId']]['name']
+        self.type = metadata.types[args['cardTypeId']]['name']
